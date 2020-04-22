767dd34fd2e2029559d4e03737f0f33ed5fdef3d
Hasan Gazi Karaşahin <hasan.karasahin@olleco.com>
2020-04-22 23:29:31 +0300

    Main Screen tasarımı Düzenleme

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -8,6 +8,7 @@ import {
   TextInput,
   TouchableOpacity,
   ImageBackground,
+  Picker,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
@@ -18,9 +19,23 @@ export default class MainScreen extends React.Component {
       <>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView style={styles.container}>
-          <View style={styles.header}>
-            <Text> Buradayım </Text>
+          <View style={styles.headerArea}>
+            <Text style={styles.headerTitle}> Translator </Text>
           </View>
+          <View style={styles.inputTargetArea}>
+            <TextInput
+              style={styles.inputTargetText}
+              numberOfLines={50}
+              multiline={true}
+            />
+          </View>
+          <View style={styles.options}>
+            <View style={styles.optionTargetLanguages} />
+            <View style={styles.optionButton} />
+            <View style={styles.optionResultLanguages} />
+          </View>
+          <View style={styles.inputResultArea} />
+          <View style={styles.translateButton} />
         </SafeAreaView>
       </>
     );
@@ -31,6 +46,59 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff',
-    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  headerArea: {
+    flex: 0.5,
+  },
+  headerTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  inputTargetArea: {
+    flex: 2,
+    backgroundColor: 'white',
+    marginBottom: 5,
+    width: '100%',
+  },
+  inputTargetText: {
+    flex: 1,
+    borderColor: 'gray',
+    borderWidth: 1,
+    shadowColor: 'black',
+    borderRadius: 10,
+    fontSize: 20,
+  },
+  options: {
+    flex: 0.5,
+    flexDirection: 'row',
+    backgroundColor: 'yellow',
+    marginBottom: 5,
+    width: '100%',
+  },
+  optionTargetLanguages: {
+    flex: 2,
+    backgroundColor: 'black',
+  },
+  optionButton: {
+    flex: 2,
+    backgroundColor: 'red',
+  },
+  optionResultLanguages: {
+    flex: 2,
+    backgroundColor: 'purple',
+  },
+  inputResultArea: {
+    flex: 2,
+    backgroundColor: 'orange',
+    marginBottom: 5,
+    width: '100%',
+  },
+  picker: {},
+  translateButton: {
+    flex: 0.5,
+    backgroundColor: 'purple',
+    width: '100%',
   },
 });
