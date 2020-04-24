83c6de4f80e15683f19ef30beceb6a77b4fac92b
Hasan Gazi Karaşahin <hasan.karasahin@olleco.com>
2020-04-24 05:04:26 +0300

    Animasyonlu component eklendi.

---
+++ src/components/FloatingLabelInput.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  Picker,
+  Button,
+  Image,
+  Alert,
+} from 'react-native';
+
+class FloatingLabelInput extends React.Component {
+  state = {
+    isFocused: false,
+  };
+
+  handleFocus = () => this.setState({isFocused: true});
+  handleBlur = () => this.setState({isFocused: false});
+
+  render() {
+    const {label, value, ...props} = this.props;
+    const {isFocused} = this.state;
+    const labelStyle = {
+      position: 'absolute',
+      left: 0,
+      top: -5,
+      fontSize: !isFocused ? 20 : 14,
+      color: !isFocused ? '#003f5c' : '#cc222a',
+    };
+    return (
+      <View style={styles.inputView}>
+        <Text style={labelStyle}>{label}</Text>
+        <TextInput
+          {...props}
+          style={{
+            height: 50,
+            fontSize: 20,
+            color: '#000',
+            borderBottomWidth: 2,
+            borderBottomColor: '#555',
+          }}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  inputView: {
+    borderRadius: 10,
+    borderColor: '#000000',
+    borderWidth: 0,
+    justifyContent: 'center',
+  },
+});
+
+export default FloatingLabelInput;
